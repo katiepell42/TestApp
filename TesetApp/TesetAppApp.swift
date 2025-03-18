1a167b55c3f505f1c579c25919163e4a8d84e726
Katie Pell <57158239+katiepell42@users.noreply.github.com>
2025-03-17 21:19:30 -0500

    Initial Commit

---
+++ TesetApp/TesetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesetAppApp.swift
+//  TesetApp
+//
+//  Created by Katie Pell on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct TesetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
